77c3efe3bfcc20eead800fdedd2a7eabd69bc2ef
Namiq157 <mnamiq188@gmail.com>
2024-12-15 02:22:58 +0400

    first1

---
+++ src/Components/ContactInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactInfo = () => {
+  return (
+    <div>
+      <h1>Əlaqə Məlumatları</h1>
+      <p>Tel +994104441057</p>
+    </div>
+  );
+};
+
+export default ContactInfo;
